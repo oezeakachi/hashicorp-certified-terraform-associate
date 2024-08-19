6f0f51edc120ce10bc8629a08346cd9293ff1436
Obi Ezeakachi <oezeakac@oezeakac-thinkpadt14gen3.remote.csb>
2024-08-20 00:58:17 +0100

    Added changes and got working

--- 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/c3-ec2-instance.tf
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/c3-ec2-instance.tf
@@ -1,8 +1,47 @@
+## SSH Key Creation
+
+## Output variables
+output "ssh_keypair" {
+  value = tls_private_key.key.private_key_pem
+  sensitive = true
+}
+
+
+output "key_name" {
+  value = aws_key_pair.key_pair.key_name
+  
+}
+
+## variable definition
+variable "key_name_definer" {
+  description = ""
+  default     = "LL-TEST"
+  type        = string
+}
+
+
+## Key Pair Creation#######################################
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key" {
+  filename          = "${var.key_name_definer}-key.pem"
+  sensitive_content = tls_private_key.key.private_key_pem
+  file_permission   = "0400"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.key_name_definer}-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
+############################################################
+
 # Resource-8: Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
-  ami                    = "ami-047a51fa27710816e" # Amazon Linux
+  ami                    = "ami-07d4917b6f95f5c2a" # Amazon Linux
   instance_type          = "t2.micro"
-  key_name               = "terraform-key"
+  key_name               = "${var.key_name_definer}-key"
   subnet_id              = aws_subnet.vpc-dev-public-subnet-1.id
   vpc_security_group_ids = [aws_security_group.dev-vpc-sg.id]
   #user_data = file("apache-install.sh")
