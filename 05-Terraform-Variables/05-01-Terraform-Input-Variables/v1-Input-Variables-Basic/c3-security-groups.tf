5dff353f21881175106a9b0395ea6664f58ad516
Obinna Ezeakachi <oezeakac@redhat.com>
2024-12-18 16:58:41 +0000

    works

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c3-security-groups.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c3-security-groups.tf
@@ -1,7 +1,51 @@
+# Resources Block
+# Resource-1: Create VPC
+resource "aws_vpc" "vpc-dev" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "vpc-dev"
+  }
+}
+
+# Resource-2: Create Subnets
+resource "aws_subnet" "vpc-dev-public-subnet-1" {
+  vpc_id                  = aws_vpc.vpc-dev.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-1a"
+  map_public_ip_on_launch = true
+}
+
+# Resource-3: Internet Gateway
+resource "aws_internet_gateway" "vpc-dev-igw" {
+  vpc_id = aws_vpc.vpc-dev.id
+}
+
+# Resource-4: Create Route Table
+resource "aws_route_table" "vpc-dev-public-route-table" {
+  vpc_id = aws_vpc.vpc-dev.id
+}
+
+# Resource-5: Create Route in Route Table for Internet Access
+resource "aws_route" "vpc-dev-public-route" {
+  route_table_id         = aws_route_table.vpc-dev-public-route-table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc-dev-igw.id
+}
+
+# Resource-6: Associate the Route Table with the Subnet
+resource "aws_route_table_association" "vpc-dev-public-route-table-associate" {
+  route_table_id = aws_route_table.vpc-dev-public-route-table.id
+  subnet_id      = aws_subnet.vpc-dev-public-subnet-1.id
+}
+
+# Resource-7: Create Security Group
+
+
 # Create Security Group - SSH Traffic
 resource "aws_security_group" "vpc-ssh" {
   name        = "vpc-ssh"
   description = "Dev VPC SSH"
+  vpc_id      = aws_vpc.vpc-dev.id
   ingress {
     description = "Allow Port 22"
     from_port   = 22
@@ -22,6 +66,7 @@ resource "aws_security_group" "vpc-ssh" {
 resource "aws_security_group" "vpc-web" {
   name        = "vpc-web"
   description = "Dev VPC Web"
+  vpc_id      = aws_vpc.vpc-dev.id
 
   ingress {
     description = "Allow Port 80"
--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c4-ec2-instance.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c4-ec2-instance.tf
@@ -1,9 +1,49 @@
+## SSH Key Creation
+
+## Output variables
+output "ssh_keypair" {
+  value = tls_private_key.key.private_key_pem
+  sensitive = true
+}
+
+
+output "key_name" {
+  value = aws_key_pair.key_pair.key_name
+  
+}
+
+## variable definition
+variable "key_name_definer" {
+  description = ""
+  default     = "LL-TEST"
+  type        = string
+}
+
+
+## Key Pair Creation#######################################
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key" {
+  filename          = "${var.key_name_definer}-key.pem"
+  sensitive_content = tls_private_key.key.private_key_pem
+  file_permission   = "0400"
+}
+
+resource "aws_key_pair" "key_pair" {
+  key_name   = "${var.key_name_definer}-key"
+  public_key = tls_private_key.key.public_key_openssh
+}
+############################################################
+
 # Create EC2 Instance
 resource "aws_instance" "my-ec2-vm" {
   ami                    = var.ec2_ami_id
   instance_type          = "t3.micro"
-  key_name               = "terraform-key"
-  count                  = var.ec2_instance_count
+  key_name               = "${var.key_name_definer}-key"
+  count                  = var.ec2_instance_count 
+  subnet_id     = aws_subnet.vpc-dev-public-subnet-1.id
   user_data              = <<-EOF
     #!/bin/bash
     sudo yum update -y
